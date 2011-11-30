35d45ba197e00e28343dda5f912cfdc5cc057ab4
Ben Pickles <spideryoung@gmail.com>
2011-11-30 18:04:20 +0000

    Call it like I see it.

--- jquery.peity.js
+++ jquery.peity.js
@@ -57,24 +57,24 @@
       var adjust = -Math.PI / 2;
       var slice = (v1 / v2) * Math.PI * 2;
 
-      var elem = createCanvas(opts.radius, opts.radius)
-      var canvas = elem.getContext("2d");
+      var canvas = createCanvas(opts.radius, opts.radius)
+      var context = canvas.getContext("2d");
 
       // Plate.
-      canvas.beginPath();
-      canvas.moveTo(centre, centre);
-      canvas.arc(centre, centre, centre, slice + adjust, (slice == 0) ? Math.PI * 2 : adjust, false);
-      canvas.fillStyle = opts.colours[0];
-      canvas.fill();
+      context.beginPath();
+      context.moveTo(centre, centre);
+      context.arc(centre, centre, centre, slice + adjust, (slice == 0) ? Math.PI * 2 : adjust, false);
+      context.fillStyle = opts.colours[0];
+      context.fill();
 
       // Slice of pie.
-      canvas.beginPath();
-      canvas.moveTo(centre, centre);
-      canvas.arc(centre, centre, centre, adjust, slice + adjust, false);
-      canvas.fillStyle = opts.colours[1];
-      canvas.fill();
+      context.beginPath();
+      context.moveTo(centre, centre);
+      context.arc(centre, centre, centre, adjust, slice + adjust, false);
+      context.fillStyle = opts.colours[1];
+      context.fill();
 
-      $this.wrapInner($("<span>").hide()).append(elem)
+      $this.wrapInner($("<span>").hide()).append(canvas)
   });
 
   peity.add(
@@ -90,7 +90,7 @@
     },
     function(opts) {
       var $this = $(this)
-      var elem = createCanvas(opts.width, opts.height)
+      var canvas = createCanvas(opts.width, opts.height)
       var values = $this.text().split(opts.delimeter)
       if (values.length == 1) values.push(values[0])
       var max = Math.max.apply(Math, values.concat([opts.max]));
@@ -99,32 +99,32 @@
       var coords = [];
       var i;
 
-      var canvas = elem.getContext("2d");
-      canvas.beginPath();
-      canvas.moveTo(0, opts.height);
+      var context = canvas.getContext("2d");
+      context.beginPath();
+      context.moveTo(0, opts.height);
 
       for (i = 0; i < values.length; i++) {
         var height = ratio * values[i];
         var x = i * width;
         var y = opts.height - height;
         coords.push({ x: x, y: y });
-        canvas.lineTo(x, y);
+        context.lineTo(x, y);
       }
 
-      canvas.lineTo(opts.width, opts.height);
-      canvas.fillStyle = opts.colour;
-      canvas.fill();
+      context.lineTo(opts.width, opts.height);
+      context.fillStyle = opts.colour;
+      context.fill();
 
-      canvas.beginPath();
-      canvas.moveTo(0, coords[0].y);
+      context.beginPath();
+      context.moveTo(0, coords[0].y);
       for (i = 0; i < coords.length; i++) {
-        canvas.lineTo(coords[i].x, coords[i].y);
+        context.lineTo(coords[i].x, coords[i].y);
       }
-      canvas.lineWidth = opts.strokeWidth;
-      canvas.strokeStyle = opts.strokeColour;
-      canvas.stroke();
+      context.lineWidth = opts.strokeWidth;
+      context.strokeStyle = opts.strokeColour;
+      context.stroke();
 
-      $this.wrapInner($("<span>").hide()).append(elem)
+      $this.wrapInner($("<span>").hide()).append(canvas)
     }
   );
 
@@ -139,24 +139,24 @@
     },
     function(opts) {
       var $this = $(this)
-      var elem = createCanvas(opts.width, opts.height)
+      var canvas = createCanvas(opts.width, opts.height)
       var values = $this.text().split(opts.delimeter)
       var max = Math.max.apply(Math, values.concat([opts.max]));
       var ratio = opts.height / max;
       var width = opts.width / values.length;
 
-      var canvas = elem.getContext("2d");
-      canvas.fillStyle = opts.colour;
+      var context = canvas.getContext("2d");
+      context.fillStyle = opts.colour;
 
       for (var i = 0; i < values.length; i++) {
         var height = ratio * values[i];
         var x = i * width;
         var y = opts.height - height;
 
-        canvas.fillRect(x, y, width, height);
+        context.fillRect(x, y, width, height);
       }
 
-      $this.wrapInner($("<span>").hide()).append(elem)
+      $this.wrapInner($("<span>").hide()).append(canvas)
     }
   );
 })(jQuery, document);
